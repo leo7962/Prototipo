c5857dd1a6c465a079e96edaf5d0dfb075443ae3
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-05-16 19:54:34 -0500

    se realizan cambios

--- Prototipo.ino
+++ Prototipo.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
 //#include <WiFiClientSecure.h>
 
 //-------------------VARIABLES GLOBALES--------------------------
@@ -12,14 +15,29 @@ int contconexion = 0;
 const char *ssid = "UMB_Docentes";
 const char *password = "Docentes2017!";
 
-char host[48];
+mongocxx::instance inst;
+mongocxx::client conn
+{
+  mongocxx::uri { "mongodb+srv://Leo7962:XperiaX7962@clusterumb-8hedx.gcp.mongodb.net/test?retryWrites=true" }
+}
+mongocxx::database db = conn["test"];
+
+try
+{
+  status = client.admin.command("serverStatus");
+  serial.println("Conectado a Mongo");
+}
+catch(const std::exception& e)
+{
+  std::cerr << e.what() << '\n';
+}
 
 
 //-------- Fin de Variables------------------
-
-void setup() {
- Serial.begin(115200);
- pinMode(inputPin,INPUT);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(inputPin, INPUT);
 
 }
 int c=0;
